c857bb41134b1b6464810524575281548bc02f03
Mathias Cousté <mathiascouste@users.noreply.github.com>
2019-12-05 21:00:42 +0100

    Initial commit

---
+++ src/main/java/fr/unice/polytech/si3/qgl/teamid/Cockpit.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.si3.qgl.teamid;
+
+public class Cockpit {
+
+	public void initGame(String game) {
+		System.out.println("Init game input: " + game);
+	}
+
+	public String nextRound(String round) {
+		System.out.println("Next round input: " + round);
+		return "[]";
+	}
+}
