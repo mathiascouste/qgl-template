dfee26a3a534ea359639447fc8fda703c5387ce5
mcouste <couste.mathias@gmail.com>
2020-01-15 19:22:16 +0100

    Adding usage of common interface

--- src/main/java/fr/unice/polytech/si3/qgl/teamid/Cockpit.java
+++ src/main/java/fr/unice/polytech/si3/qgl/teamid/Cockpit.java
@@ -1,6 +1,11 @@
 package fr.unice.polytech.si3.qgl.teamid;
 
-public class Cockpit {
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.unice.polytech.si3.qgl.regatta.cockpit.ICockpit;
+
+public class Cockpit implements ICockpit {
 
 	public void initGame(String game) {
 		System.out.println("Init game input: " + game);
@@ -9,5 +14,10 @@ public class Cockpit {
 	public String nextRound(String round) {
 		System.out.println("Next round input: " + round);
 		return "[]";
+	}
+
+	@Override
+	public List<String> getLogs() {
+		return new ArrayList<>();
 	}
 }
